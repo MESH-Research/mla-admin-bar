e02804e9810663cc21fcbd395e2cb3cc3d28b7e4
Jonathan Reeve <jon.reeve@gmail.com>
2014-07-23 14:04:21 -0400

    change link for my-account menu, fixes mlaa/cbox-mla#82

--- mla-admin-bar.php
+++ mla-admin-bar.php
@@ -106,3 +106,31 @@ function mla_add_blog_breadcrumb($admin_bar) {
 	} 
 }
 add_action('admin_bar_menu', 'mla_add_blog_breadcrumb', 12);
+
+function mla_admin_bar_change_howdy_target( $wp_admin_bar ) {
+	$user_id = get_current_user_id();
+	$current_user = wp_get_current_user();
+	$profile_url = get_edit_profile_url( $user_id );
+	if (substr($profile_url, -5) == 'edit/') { 
+		$profile_url = substr($profile_url, 0, -5); 
+	} 
+
+	if ( 0 != $user_id ) {
+		/* Add the "My Account" menu */
+		$avatar = get_avatar( $user_id, 28 );
+		$howdy = sprintf( __('Welcome, %1$s'), $current_user->display_name );
+		$class = empty( $avatar ) ? '' : 'with-avatar';
+
+		$wp_admin_bar->add_menu( array(
+			'id' => 'my-account',
+			'parent' => 'top-secondary',
+			'title' => $howdy . $avatar,
+			'href' => $profile_url,
+			'meta' => array(
+				'class' => $class,
+			),
+		) );
+
+	}
+}
+add_action( 'admin_bar_menu', 'mla_admin_bar_change_howdy_target', 11 );
